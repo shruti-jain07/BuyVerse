8ca471ce6cdf7ef326055df85b16fc24ea96ba63
shruti-jain07 <shrutijain19.2003@gmail.com>
2025-07-24 00:40:07 +0530

    Implemented and Styled Navbar, Sidebar, and Footer components

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,15 @@
+export const Footer = () => {
+  return (
+   <footer className="border-t font-medium p-3 flex justify-center">
+     <div className="flex flex-col items-center ">
+        <span className="text-lg font-semibold">BuyVerse</span>
+        <span className="text-sm text-gray-600">One Verse.Infinite Choices.</span>
+
+     </div>
+     
+    
+   </footer>
+  )
+}
+
+

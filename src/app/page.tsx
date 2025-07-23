43c1334c819bc52e942954e4184b773668370fd0
shruti-jain07 <shrutijain19.2003@gmail.com>
2025-07-23 17:44:29 +0530

    customized button,textarea,input,progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
 export default function Home() {
   return (
-    <div>
-      <Button variant="destructive">
-          HELLO!StayVerse
-      </Button>
-    </div>
+    <div className="p-4">
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button variant="elevated">
+          HELLO!BuyVerse
+        </Button>
+      </div>
+        <div>
+          <Input placeholder="I'm Input"/>
+        </div>
+        <div>
+          <Progress value={50}/>
+        </div>
+        <div>
+          <Textarea placeholder="I'm TextArea"/>
+        </div>
+        <div>
+          <Checkbox/>
+        </div>
+      </div>
+      </div>
+    
   );
 }

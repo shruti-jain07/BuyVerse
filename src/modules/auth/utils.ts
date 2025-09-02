b8433e557d15b784c1e15de38356c8629060cc34
shruti-jain07 <shrutijain19.2003@gmail.com>
2025-09-02 14:56:32 +0530

    fix: configure cookie options for production (domain, sameSite, secure)

--- src/modules/auth/utils.ts
+++ src/modules/auth/utils.ts
@@ -13,9 +13,9 @@ export const generateAuthCookie=async({
                 value:value,
                 httpOnly:true,
                 path:"/",
-                // sameSite:"none",
-                // domain:process.env.NEXT_PUBLIC_ROOT_DOMAIN,
-                // secure:process.env.NODE_ENV==="production"
+                sameSite:"none",
+                domain:process.env.NEXT_PUBLIC_ROOT_DOMAIN,
+                secure:process.env.NODE_ENV==="production"
 
             })
 }

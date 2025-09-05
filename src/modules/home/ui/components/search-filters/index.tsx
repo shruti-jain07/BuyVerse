eb198bbb937e6f12430be3f7dbb090ac65d3731a
shruti-jain07 <shrutijain19.2003@gmail.com>
2025-09-05 12:45:20 +0530

    fix:search filter

--- src/modules/home/ui/components/search-filters/index.tsx
+++ src/modules/home/ui/components/search-filters/index.tsx
@@ -6,9 +6,12 @@ import {useSuspenseQuery} from "@tanstack/react-query"
 import { useParams } from "next/navigation";
 import { DEFAULT_BG_COLOR } from "../../../constants";
 import {BreadcrumbNavigation}from "./breadcrumbs-navigation";
+import { useProductFilters } from "@/modules/products/hooks/use-product-filters";
 export const SearchFilters=()=>{
         const trpc=useTRPC();
         const {data}=useSuspenseQuery(trpc.categories.getMany.queryOptions())
+            const [filters,setFilters]=useProductFilters();
+        
         const params=useParams();
         const categoryParam=params.category as string |'undefined';
         const activeCategory=categoryParam||"all";
@@ -24,8 +27,14 @@ export const SearchFilters=()=>{
         
         return(
     <div className="px-4 lg-12 py-8 border-b flex flex-col gap-4 w-full" 
-    style={{backgroundColor:activeCategoryColor,}}>
-        <SearchInput />
+        style={{backgroundColor:activeCategoryColor,}}>
+        
+        <SearchInput 
+        defaultValue={filters.search} 
+        onChange={(value)=>setFilters({
+                        search:value
+        })} />
+        
         <div className="hidden lg:block">
         <Categories data={data} />
         </div>
--- src/modules/products/ui/views/product-list-view.tsx
+++ src/modules/products/ui/views/product-list-view.tsx
@@ -14,7 +14,9 @@ export const ProductListView=({category,tenantSlug,narrowView}:Props)=>{
         <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
                 <div className="flex flex-col lg:flex-row lg:items-center gap-y-2 lg:gap-y-0 justify-between">
                   <p className="text-2xl font-medium">Curated For You</p>
+                  
                   <ProductSort/>
+                  
                 </div>
                 <div className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-6 gap-x-12">
                   <div className="lg:col-span-2 xl:col-span-2">

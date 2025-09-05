4e06747b111242878086ee5b09dd8ec8e8407c52
shruti-jain07 <shrutijain19.2003@gmail.com>
2025-09-05 10:33:21 +0530

    fix:cookie

--- src/modules/auth/utils.ts
+++ src/modules/auth/utils.ts
@@ -13,8 +13,10 @@ export const generateAuthCookie=async({
                 value:value,
                 httpOnly:true,
                 path:"/",
+                ...(process.env.NODE_ENV!=="development" && {
                 sameSite:"none",
                 domain:process.env.NEXT_PUBLIC_ROOT_DOMAIN,
-                secure:process.env.NODE_ENV==="production",
+                secure:true,
+                })
             });
 }
